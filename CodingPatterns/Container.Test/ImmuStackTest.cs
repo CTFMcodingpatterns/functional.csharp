8ee9f6fa7f9320add2bf29352e72cf0ff72347f4
CTFMcodingpatterns <pipapopost@yahoo.de>
2015-12-27 16:30:20 +0100

    ImmuStack simple version

---
+++ CodingPatterns/Container.Test/ImmuStackTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using log4net;
+
+namespace Container.Test
+{
+    [TestFixture]
+    class ImmuStackTest
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(ImmuStackTest));
+
+        [Test]
+        public void push_abc_HeadIsC()
+        {
+            //Arrange
+            ImmuStack<string> stack0 = ImmuStack<string>.Empty();
+
+            //Act
+            ImmuStack<string> stack1 = new ImmuStack<string>("a", stack0);
+            ImmuStack<string> stack2 = new ImmuStack<string>("b", stack1);
+            ImmuStack<string> stack3 = new ImmuStack<string>("c", stack2);
+
+            //Assert
+            Logger.Debug(""
+                + "\r\nstack0.head: " + stack0.Head
+                + "\r\nstack1.head: " + stack1.Head
+                + "\r\nstack2.head: " + stack2.Head
+                + "\r\nstack3.head: " + stack3.Head
+                );
+            Assert.AreEqual(null, stack0.Head);
+            Assert.AreEqual("a", stack1.Head);
+            Assert.AreEqual("b", stack2.Head);
+            Assert.AreEqual("c", stack3.Head);
+        }
+    }
+}
---
+++ CodingPatterns/Container/ImmuStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Container
+{
+    public class ImmuStack<T>
+    {
+        public T Head { get; private set;}
+
+        public ImmuStack<T> Tail { get; private set; }
+
+        private ImmuStack()
+        {
+        }
+
+        public ImmuStack(T head, ImmuStack<T> tail)
+        {
+            this.Head = head;
+            this.Tail = tail;
+        }
+
+        public static ImmuStack<T> Empty()
+        {
+            return new ImmuStack<T>(default(T), null);
+        }
+
+    }
+}
